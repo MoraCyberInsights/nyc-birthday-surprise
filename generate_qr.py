d9c85d0537c831b163848532f51040ef755d6049
Gabriel <gabrielmora24@gmail.com>
2025-05-29 12:23:09 -0500

    removed unnecessary files

--- generate_qr.py
+++
@@ -1,8 +0,0 @@
-import qrcode
-
-# Replace with your deployed Streamlit app URL
-app_url = "https://nyc-birthday-surprise.streamlit.app/"
-
-qr = qrcode.make(app_url)
-qr.save("birthday_qr_code.png")
-print("QR code saved!")
