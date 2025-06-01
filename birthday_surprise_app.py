dd46ce855ac9595fa2be0326a10884a3673470f0
Gabriel <gabrielmora24@gmail.com>
2025-06-01 14:45:15 -0500

    update to the moddboard slideshow

--- birthday_surprise_app.py
+++ birthday_surprise_app.py
@@ -130,8 +130,6 @@ st.session_state.slide_index = slide_index
 # Show the image and caption for the current slide
 img = Image.open(image_paths[st.session_state.slide_index])
 st.image(img, use_container_width=True, caption=captions[st.session_state.slide_index])
-
-
     img = Image.open(image_paths[st.session_state.slide_index])
     st.image(img, use_container_width=True, caption=captions[st.session_state.slide_index])
 
